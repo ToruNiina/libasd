d5395def891be121ee7b2c2e52ee4693f20b84f5
ToruNiina <niina.toru.68u@gmail.com>
2017-10-05 17:34:57 +0900

    add some container dispatchers

---
+++ libasd/container_dispatcher.hpp
@@ -0,0 +1,69 @@
+#ifndef LIBASD_CONTAINER_DISPATCHER_H
+#define LIBASD_CONTAINER_DISPATCHER_H
+#include <vector>
+#include <deque>
+#include <array>
+#include <libasd/exception_thrower.hpp>
+
+namespace asd
+{
+namespace container
+{
+
+struct vec
+{
+    template<typename T>
+    struct rebind
+    {
+        typedef std::vector<T, std::allocator<T>> other;
+    };
+
+    template<typename T, typename Alloc>
+    static void resize(std::vector<T, Alloc>& cont, std::size_t N)
+    {
+        cont.resize(N);
+        return;
+    }
+};
+
+struct deq
+{
+    template<typename T>
+    struct rebind
+    {
+        typedef std::deque<T, std::allocator<T>> other;
+    };
+
+    template<typename T, typename Alloc>
+    static void resize(std::deque<T, Alloc>& cont, std::size_t N)
+    {
+        cont.resize(N);
+        return;
+    }
+};
+
+template<std::size_t N>
+struct arr
+{
+    template<typename T>
+    struct rebind
+    {
+        typedef std::array<T, N> other;
+    };
+
+    template<typename T>
+    static void resize(std::array<T, N>& cont, std::size_t M)
+    {
+        if(cont.size() < M)
+        {
+            throw_exception<std::bad_alloc>(
+                    "array(size = %) has no enough storage(size = %)",
+                    cont.size(), N);
+        }
+        return;
+    }
+};
+
+} // container
+} // asd
+#endif//LIBASD_CONTAINER_DISPATCHER_H
