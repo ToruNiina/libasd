1329266f945e9f79df6f4d455c0ab93c154ce8c7
ToruNiina <niina.toru.68u@gmail.com>
2017-11-25 19:00:08 +0900

    update convert()

--- libasd/convert.hpp
+++ libasd/convert.hpp
@@ -1,5 +1,6 @@
 #ifndef LIBASD_CONVERT_H
 #define LIBASD_CONVERT_H
+#include <libasd/exception_thrower.hpp>
 #include <libasd/header.hpp>
 #include <libasd/frame_data.hpp>
 #include <algorithm>
@@ -13,17 +14,40 @@ convert_data(const FrameData<std::int16_t, contT>& data,
              const Header<version<1>>& header, const std::size_t ch)
 {
     FrameData<realT, contT> retval(data.x_pixel(), data.y_pixel());
-//     const auto kind = (ch == 1) ? header.data_kind_1ch : header.data_kind_2ch;
-    // TODO switch(data_kind)
 
-    const std::uint64_t AD_resl = (1 << header.AD_resolution);
-    // assuming AD_range == 10
-    const realT resolution = 10.0 / AD_resl; // [V/int12_t]
-    const auto coef = resolution *
-                      header.z_piezo_gain * header.z_piezo_extension;
+    // int16_t -> voltage
+    const auto          ADrg = header.ad_range;
+    const std::uint64_t resl = (1ul << header.ad_resolution);
+
+    // voltage -> data
+    const auto kind = [ch](const Header<version<1>>& hdr)
+    {
+             if(ch == 0){return hdr.data_kind_1ch;}
+        else if(ch == 1){return hdr.data_kind_2ch;}
+        else throw_exception<std::invalid_argument>("invalid channel: %", ch);
+    }(header);
+    const auto coef = [kind](const Header<version<1>>& hdr) -> realT
+    {
+        switch(kind)
+        {
+            case data_kind::topography:
+                return hdr.z_piezo_gain * hdr.z_piezo_extension;
+            case data_kind::error:
+                return -1.0 * hdr.sensor_sensitivity;
+            case data_kind::phase:
+                return -1.0 * hdr.phase_sensitivity;
+            case data_kind::none:
+                return 0.0;
+            default:
+                throw_exception<std::invalid_argument>("invalid data_kind: %",
+                        static_cast<std::int32_t>(kind));
+        }
+    }(header);
 
     std::transform(data.raw_begin(), data.raw_end(), retval.raw_begin(),
-                   [=](const std::int16_t i){return i * coef;});
+                   [ADrg, resl, coef](const std::int16_t i){
+                       return level_to_voltage<realT>(i, ADrg, resl) * coef;
+                   });
     return retval;
 }
 
