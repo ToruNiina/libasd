17fbebe5f81a0627afd3701de0587ac3633c6a60
ToruNiina <niina.toru.68u@gmail.com>
2018-02-01 16:56:26 +0900

    change indents

--- libasd/container_dispatcher.hpp
+++ libasd/container_dispatcher.hpp
@@ -1,10 +1,10 @@
 #ifndef LIBASD_CONTAINER_DISPATCHER_H
 #define LIBASD_CONTAINER_DISPATCHER_H
+#include <libasd/exception_thrower.hpp>
+#include <libasd/container_traits.hpp>
 #include <vector>
 #include <deque>
 #include <array>
-#include <libasd/exception_thrower.hpp>
-#include <libasd/container_traits.hpp>
 
 namespace asd
 {
@@ -12,39 +12,38 @@ namespace container
 {
 
 template<typename containerT, typename std::enable_if<
-    ::asd::detail::has_mem_func_data<containerT>::value, std::nullptr_t
-    >::type = nullptr>
-inline typename containerT::value_type const* get_ptr(const containerT& c)
-    noexcept(noexcept(c.data()))
+    ::asd::detail::has_mem_func_data<containerT>::value,
+    std::nullptr_t>::type = nullptr>
+inline typename container_traits<containerT>::value_type const*
+get_ptr(const containerT& c) noexcept(noexcept(c.data()))
 {
     return c.data();
 }
 
 template<typename containerT, typename std::enable_if<
-    ::asd::detail::has_mem_func_size<containerT>::value, std::nullptr_t
-    >::type = nullptr>
+    ::asd::detail::has_mem_func_size<containerT>::value,
+    std::nullptr_t>::type = nullptr>
 inline std::size_t size(const containerT& c) noexcept(noexcept(c.size()))
 {
     return c.size();
 }
 
 template<typename containerT, typename std::enable_if<
-    ::asd::detail::has_mem_func_resize<containerT>::value, std::nullptr_t
-    >::type = nullptr>
+    ::asd::detail::has_mem_func_resize<containerT>::value,
+    std::nullptr_t>::type = nullptr>
 inline void resize(containerT& c, std::size_t N)
 {
-    return c.resize(N);
+    c.resize(N);
+    return;
 }
 
 template<typename containerT, typename std::enable_if<
-    ::asd::detail::has_mem_func_clear<containerT>::value, std::nullptr_t
-    >::type = nullptr>
+    ::asd::detail::has_mem_func_clear<containerT>::value,
+    std::nullptr_t>::type = nullptr>
 inline void clear(containerT& c) noexcept(noexcept(c.clear()))
 {
     return c.clear();
 }
-
-// ---------------------------------- vector ---------------------------------//
 
 struct vec
 {
@@ -55,8 +54,6 @@ struct vec
     };
 };
 
-// ---------------------------------- deque ----------------------------------//
-
 struct deq
 {
     template<typename T>
@@ -65,8 +62,6 @@ struct deq
         typedef std::deque<T, std::allocator<T>> other;
     };
 };
-
-// ---------------------------------- array ----------------------------------//
 
 template<std::size_t N>
 struct arr
