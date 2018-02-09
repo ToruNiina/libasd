43cf8aa715721de92e715ebbdbc055e168d1eb31
ToruNiina <niina.toru.68u@gmail.com>
2018-02-09 18:57:16 +0900

    fix path to include files

--- libasd.hpp
+++ libasd.hpp
@@ -1,8 +1,8 @@
 #ifndef LIBASD_LIBASD_ROOT_HEADER
 #define LIBASD_LIBASD_ROOT_HEADER
 
-#include <libasd/io/data.hpp>
-#include <libasd/io/read_asd.hpp>
-#include <libasd/io/write_asd.hpp>
+#include <libasd/data.hpp>
+#include <libasd/read_asd.hpp>
+#include <libasd/write_asd.hpp>
 
 #endif// LIBASD_LIBASD_ROOT_HEADER
