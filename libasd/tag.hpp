9eeadd32289edb3a13b77591f4f3269064c25481
ToruNiina <niina.toru.68u@gmail.com>
2021-04-07 12:23:32 +0900

    :sparkles: add a shortcut to a tag
    if C++17 features are enabled

--- libasd/tag.hpp
+++ libasd/tag.hpp
@@ -14,5 +14,13 @@ struct version : std::integral_constant<std::size_t, N>{};
 template<std::size_t N> using ch  = channel<N>;
 template<std::size_t N> using ver = version<N>;
 
+#if __cplusplus >= 201703L
+constexpr inline auto ch1 = channel<1>;
+constexpr inline auto ch2 = channel<2>;
+constexpr inline auto v0  = version<0>;
+constexpr inline auto v1  = version<1>;
+constexpr inline auto v2  = version<2>;
+#endif
+
 } // asd
 #endif//LIBASD_CHANNEL_TAG_H
