49da5921f69e96eccd121289633d13b58a5f86cf
ToruNiina <niina.toru.68u@gmail.com>
2021-12-22 01:42:48 +0900

    fix #10: add missing aggregate initializer

--- libasd/tag.hpp
+++ libasd/tag.hpp
@@ -15,11 +15,11 @@ template<std::size_t N> using ch  = channel<N>;
 template<std::size_t N> using ver = version<N>;
 
 #if __cplusplus >= 201703L
-constexpr inline auto ch1 = channel<1>;
-constexpr inline auto ch2 = channel<2>;
-constexpr inline auto v0  = version<0>;
-constexpr inline auto v1  = version<1>;
-constexpr inline auto v2  = version<2>;
+constexpr inline auto ch1 = channel<1>{};
+constexpr inline auto ch2 = channel<2>{};
+constexpr inline auto v0  = version<0>{};
+constexpr inline auto v1  = version<1>{};
+constexpr inline auto v2  = version<2>{};
 #endif
 
 } // asd
